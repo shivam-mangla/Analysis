e8c1e731ca267270c080d5d5aac48c49df4ff108
Shivam <shivam.mangla.09@gmail.com>
2014-12-19 13:37:29 +0530

    Add code to get browser history and update indentation

--- analysis_app/src/in/mdgiitr/analysis/MainActivity.java
+++ analysis_app/src/in/mdgiitr/analysis/MainActivity.java
@@ -1,64 +1,86 @@
 package in.mdgiitr.analysis;
 
 import android.app.Activity;
-import android.app.ActionBar;
 import android.app.Fragment;
+import android.database.Cursor;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.provider.Browser;
+import android.util.Log;
+import android.view.*;
 
 public class MainActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+    private static final String DEBUG_TAG = "MainActiivty ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment()).commit();
-		}
-	}
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+        getBrowserHistory();
+    }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
 
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
-	}
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
 
-	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
-		}
-		return super.onOptionsItemSelected(item);
-	}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
 
-		public PlaceholderFragment() {
-		}
+        public PlaceholderFragment() {
+        }
 
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container,
-					false);
-			return rootView;
-		}
-	}
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container,
+                    false);
+            return rootView;
+        }
+    }
+
+    public void getBrowserHistory() {
+        String[] proj = new String[]{Browser.BookmarkColumns.TITLE, Browser.BookmarkColumns.URL,
+                Browser.BookmarkColumns.DATE, Browser.BookmarkColumns.VISITS};
+
+        // Check other attirbutes in BookmarkColumns
+        String sel = Browser.BookmarkColumns.BOOKMARK + " = 0"; // 0 = history, 1 = bookmark
+        Cursor mCur = getContentResolver().query(Browser.BOOKMARKS_URI, proj, sel, null, null);
+        mCur.moveToFirst();
+        String title = "", url = "", date = "", visits = "";
+        if (mCur.moveToFirst() && mCur.getCount() > 0) {
+            boolean cont = true;
+            while (mCur.isAfterLast() == false && cont) {
+                title = mCur.getString(mCur.getColumnIndex(proj[0]));
+                url = mCur.getString(mCur.getColumnIndex(proj[1]));
+                date = mCur.getString(mCur.getColumnIndex(proj[2]));
+                visits = mCur.getString(mCur.getColumnIndex(proj[3]));
+                Log.i(DEBUG_TAG + "getBrowserHistory = ", date + ", " + visits + ", " + title + ", " + url + ", ");
+                // Do something with title and url
+                mCur.moveToNext();
+            }
+        }
+    }
 
 }
